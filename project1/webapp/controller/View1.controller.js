de746a050f84a9ad900f225b14f0e14b25edb7fb
rkmantupar <128513191+rkmantupar@users.noreply.github.com>
2025-06-02 09:43:09 +0800

    Update View1.controller.js
    Update clear button

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -24,22 +24,15 @@ function(Controller) {
             console.log(oCountry);
         },
 
-        onClearBtn: function(oEvent) {
-            var oView    = this.getView();
-            var sName    = oView.byId("NameInput").getValue();
-            var sStreet  = oView.byId("StreetInput").getValue();
-            var sNo      = oView.byId("NoInput").getValue();
-            var sZipCode = oView.byId("ZipCodeInput").getValue();
-            var sCity    = oView.byId("CityInput").getValue();
-            var oCountry = oView.byId("CountryInput").getValue();
-            
-            console.log(oView);
-            console.log(sName);
-            console.log(sStreet);
-            console.log(sNo);
-            console.log(sZipCode);
-            console.log(sCity);
-            console.log(oCountry);
+        onClearBtn: function (oEvent) {
+            var oDelete = this.getView();
+            oDelete.byId("NameInput").setValue();
+            oDelete.byId("StreetInput").setValue();
+            oDelete.byId("StreetInput").setValue();
+            oDelete.byId("NoInput").setValue();
+            oDelete.byId("ZipCodeInput").setValue();
+            oDelete.byId("CityInput").setValue();
+            oDelete.byId("CountryInput").setValue(); 
         }
     });
 });
